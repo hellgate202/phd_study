911972f25a030a1ab9de02d8acb67b9766918b0b
Leonid Ivanov <l.ivanov@metrotek.ru>
2019-08-26 16:08:11 +0300

    trying  to estimate motion vectors

--- img_tutor.py
+++ img_tutor.py
@@ -2,8 +2,8 @@ import numpy as np
 import cv2
 import sys
 
-texture_bs = 5
-texture_thr = 100
+threshold = 90
+blocksize = 17
 
 img = cv2.imread( sys.argv[1], 1 )
 y_img = cv2.cvtColor( img, cv2.COLOR_BGR2GRAY )
@@ -18,24 +18,31 @@ def multiscaleMorphGrad( img, k_list ):
     m_img_list.append( m_img.astype( np.uint16 ) )
   for i in range( len( k_list ) ):
     s_img = sum( m_img_list ) / len( m_img_list )
-  return( s_img.astype( np.uint8 ) ) 
+  return s_img 
 
     
-m_img = multiscaleMorphGrad( y_img, [3,5,7,9,11] )
+m_img = multiscaleMorphGrad( y_img, [3,5,7] )
 
-bs_delta = texture_bs // 2
-y_blocks_amount = y_size // texture_bs
-x_blocks_amount = x_size // texture_bs
-for y in range( bs_delta, y_blocks_amount, texture_bs ):
-  for x in range( bs_delta, x_blocks_amount, texture_bs ):
-    block = m_img[x - bs_delta : x + bs_delta + 1, y - bs_delta : y + bs_delta + 1]
-    texture_value = sum( sum( block ) )
-    print( block.dtype )
+def findHighFreqBlocks( img, bs, threshold ):
+  px_in_block = bs ** 2
+  bs_delta = bs // 2
+  y_size = img.shape[0]
+  x_size = img.shape[1]
+  y_blocks_amount = y_size // bs
+  x_blocks_amount = x_size // bs
+  y_scan = y_blocks_amount * bs
+  x_scan = x_blocks_amount * bs
+  o_img = img
+  for y in range( bs_delta, y_scan, bs ):
+    for x in range( bs_delta, x_scan, bs ):
+      block = m_img[y - bs_delta : y + bs_delta + 1, x - bs_delta : x + bs_delta + 1]
+      texture_value = sum( sum( block ) ) / px_in_block
+      if( texture_value > threshold ):
+        cv2.rectangle( o_img, (x + bs_delta,y - bs_delta), (x - bs_delta,y + bs_delta), (255,255,255), 1)
+  return o_img
 
+m_img = findHighFreqBlocks( m_img, blocksize, threshold )
 
-'''
 cv2.imshow( 'm_img', m_img.astype( np.uint8 ) )
-
 cv2.waitKey( 0 )
 cv2.destroyAllWindows( )
-'''
