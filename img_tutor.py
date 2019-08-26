a54c0e22ef761c9616ee23b0800ab1c8a288a178
Leonid Ivanov <l.ivanov@metrotek.ru>
2019-08-26 10:54:03 +0300

    multiscale morph gradient

--- img_tutor.py
+++ img_tutor.py
@@ -3,15 +3,23 @@ import cv2
 import sys
 
 img = cv2.imread( sys.argv[1], 1 )
-n_img = np.zeros( (img.shape[0], img.shape[1]) )
+y_img = cv2.cvtColor( img, cv2.COLOR_BGR2GRAY )
 
-for y in range( img.shape[0] ):
-  for x in range( img.shape[1] ):
-    y_val = img.item(y,x,0)*0.114+img.item(y,x,1)*0.587+img.item(y,x,2)*0.299
-    n_img.itemset((y,x),y_val)
-n_img = n_img.astype( np.uint8 )
+def multiscaleMorphGrad( img, k_list ):
+  m_img_list = []
+  for i in range( len( k_list ) ):
+    kernel = cv2.getStructuringElement( cv2.MORPH_RECT, (k_list[i],k_list[i]) )
+    m_img = cv2.morphologyEx( img, cv2.MORPH_GRADIENT, kernel )
+    m_img_list.append( m_img.astype( np.uint16 ) )
+  for i in range( len( k_list ) ):
+    s_img = sum( m_img_list ) / len( m_img_list )
+  return( s_img.astype( np.uint8 ) )
 
-cv2.imshow( 'image', n_img )
+    
+m_img = multiscaleMorphGrad( y_img, [3,5,7,9,11] )
+
+cv2.imshow( 'm_img', m_img.astype( np.uint8 ) )
+
 cv2.waitKey( 0 )
-cv2.destroyWindow( 'image' )
+cv2.destroyAllWindows( )
 
