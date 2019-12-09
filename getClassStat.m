83cc979ceed83db513e26f90a38e5849d7c454e8
Leonid Ivanov <l.ivanov@metrotek.ru>
2019-12-09 23:28:25 +0300

    fixed elipses

--- getClassStat.m
+++ getClassStat.m
@@ -5,13 +5,17 @@ function [class_cov, mean_x, mean_y] = getClassStat( classes, class_num, block_s
   for y = 1 : 1 : y_size
     for x = 1 : 1 : x_size
       if classes(y,x) == class_num
-        x_vec = [x_vec x];
-        y_vec = [y_vec y];
+        x_vec = [x_vec ( x - 1 ) * block_size + 1];
+        y_vec = [y_vec ( y - 1 ) * block_size + 1];
+        x_vec = [x_vec  x * block_size];
+        y_vec = [y_vec ( y - 1 ) * block_size + 1];
+        x_vec = [x_vec  x * block_size];
+        y_vec = [y_vec  y * block_size];
+        x_vec = [x_vec ( x - 1 ) * block_size + 1];
+        y_vec = [y_vec  y * block_size];
       end
     end
   end
-  x_vec = ( x_vec - 1 ) .* block_size + 1;
-  y_vec = ( y_vec - 1 ) .* block_size + 1;
   class_cov = cov( x_vec, y_vec );
   mean_x = mean( x_vec );
   mean_y = mean( y_vec );
